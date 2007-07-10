8db3d5b5b1b9794325dc34a80279db4aec3a23b6
vikraman <vikraman@8f67dd55-be34-0410-afa1-fda33ee09a7f>
2007-07-10 13:04:45 +0000

    updated version

--- lotb_render.py
+++ lotb_render.py
@@ -1,17 +1,37 @@
-class Game:
+class SplEffects:
 	def __init__(self):
+		self.x = 50
+		self.x1=10
+	def draw(self,c):		
+		c.after(200,self.sample)
+	def sample(self):
+		self.x =self.x+self.x1
+		c.create_rectangle(self.x,0,100,100,fill='#00ff00')
+		c.pack()
+		c.after(200,self.sample)
+
+		
+
+class Game:
+	global s
+	s = SplEffects()
+	def __init__(self,canvas):
 		self.count = 0
 		self.layout =""
 		self.teams=[]
+		self.canvas = canvas
+		s.draw(self.canvas)
 
 	def handle_rawText(self,raw_text):
 		# Every turn has a number written in its 0th position except the first 5 lines
 		self.count+=1 			
-
+		if self.count>5:
+			pass
+			
 		if self.count ==1 and raw_text!="LOTB.DUMP.1": return "GameOver" 
 		if self.count ==2: # The actual map
 			#Here we expect the map
-			self.layout = Layout(raw_text,300,300) #values to be replaced later.
+			self.layout = Layout(raw_text,self.canvas) #values to be replaced later.
 		if self.count ==3: #Ammo and health positions
 			self.layout.layout_change(raw_text)
 
@@ -20,13 +40,12 @@ class Game:
 			count = -1
 			for team in eval(raw_text):
 				count +=1
-				self.teams.append(Team(team,count))
+				self.teams.append(Team(team,colour_pref[count]))
 		if self.count ==5: #List of bots
 			for bot_dict in eval(raw_text):
 				bot = Bot(bot_dict['n'],bot_dict['t'])
 				for team in self.teams: 
 					if team.team_name == bot_dict['t']: team.bot_list.append(bot)
-
 
 			
 	
@@ -34,16 +53,21 @@ class Game:
 
 			
 class Layout:
-	def __init__(self,map,width,height): 
+	def __init__(self,map,canvas): 
 		# screen width and height to calculate main layout and insect map layout
 		self.original_map = eval(map)
 		self.present_map = eval(map)		
-		self.insx = round(width*(7/8)) # The insect map,insx n insy, will occupy 1/8th of the screen
-		self.insy = round(height*(1/8))
+	#	self.insx = round(width*(7/8)) # The insect map,insx n insy, will occupy 1/8th of the screen
+	#	self.insy = round(height*(1/8))
 		self.mainx =0
 		self.mainy =0
+		self.c = canvas
+	#	print self.canvas.width,self.canvas.height
 		self.cell_width =32 # The logic of filling this shall be changed later
+
+
 	def render(self,present_map):
+	 	self.c.create_rectangle(0, 0, 200, 300, fill="#ff0000")		
 		pass
 		# Here the code goes for rendering the map on screen.
 		# This automatically renders the insect map.
@@ -66,8 +90,10 @@ class Team:
 	# The team contains the list of bots which belong to it
 	def __init__(self,t_name,colour):
 		self.team_name = t_name
-		self.team_color = colour
+		self.team_colour = colour
 		self.bot_list = []
+		print "Team created",self.team_name,self.team_colour
+
 	def add_bot(self,bot): # the bot object created by Game Class is passed here to be appended to TeamClass
 		self.bot_list.append(bot)
 
@@ -79,12 +105,47 @@ class Bot:
 		self.health = 0
 		self.ammo = 0
 		self.kills =0
-		print "Bot Created",self.name,"\nTeam:",self.team_name
 		self.list_of_actions = [] # this will be a repository of all that the bot did in the game.
 
-g = Game()
+from Tkinter import *
+import winsound
+import os
+import threading
+
+class MyThread(threading.Thread):
+	def draw(self,c,root):
+		c.pack()
+		root.mainloop()
+
+class SampleThread(threading.Thread):
+	def draw(self,c):
+		count = 0
+		print "i am herer"
+		while 1:
+			count +=1
+			col = ['#00ff00','#0000ff','#ff0000']
+			c.after(200)
+			c.create_rectangle(0,0,100+count,100,fill=col[count%3])
+			c.pack()
+			c.after(200)
+
+
+
+#----The Canvas is created here and passed to Game class --------
+root = Tk()
+c = Canvas(root, width=800,height=600)
+root.title("Lord of the Bots")
+c.pack()
+g = Game(c)
+#th1 = SampleThread()
+#th1.draw(c)
+#th = MyThread()
+#th.draw(c,root)
+
+root.mainloop()
+#----The width,height shall be replaced with "find_monitor_resolution()
+
 file = open('simlog.txt','r')
 for line in file.readlines():
 	g.handle_rawText(line)
-
 
